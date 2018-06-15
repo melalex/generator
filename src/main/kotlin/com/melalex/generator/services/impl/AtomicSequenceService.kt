6fcba77735cf4ee3b34a9f9c91dc837d3afc8d0d
Oleksandr_Melashchenko <Oleksandr_Melashchenko@epam.com>
2018-06-15 16:32:07 +0300

    Add init value

--- src/main/kotlin/com/melalex/generator/services/impl/AtomicSequenceService.kt
+++ src/main/kotlin/com/melalex/generator/services/impl/AtomicSequenceService.kt
@@ -3,7 +3,7 @@ package com.melalex.generator.services.impl
 import com.melalex.generator.services.SequenceService
 import java.util.concurrent.atomic.AtomicLong
 
-class AtomicSequenceService(initValue: Long) : SequenceService {
+class AtomicSequenceService(initValue: Long = 0) : SequenceService {
 
     private val counter: AtomicLong = AtomicLong(initValue)
 
